df1cef692354e84273e3d4fecc56e1739aa6c264
Oleg Esaulov <esaulov.o.k@gmail.com>
2022-02-18 14:20:03 +0300

    section posts. Initial version

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
 import * as commonFunctions from "./modules/functions.js";
 import "./modules/burger.js";
+import { Carousel } from "./modules/carousel/carousel.js";
 
 commonFunctions.isWebp();
+const carousel = new Carousel({
+    prefix: "slider",
+    showArrows: false,
+    autoplaySpeed: 8000,
+});
